95deecb720168d6c790f9fedd652371b48fa2b60
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-05-15 21:57:15 +1200

    add new problem about merging lists

---
+++ 2022/May/MergeLists.js
@@ -0,0 +1,29 @@
+/*
+Given pointers to the heads of two sorted linked lists, merge them into a single, sorted linked list.
+Either head pointer may be null, meaning that the corresponding list is empty.
+
+Example
+headA refers to 1 - 3 - 7 - null
+headB refers to 1 - 2 - null
+A & B both are references to the head of a list
+The new list is 1 - 1 -2 - 3 - 7 - null
+
+Returns SinglyLinkedListNode pointer: a reference to the head of the merged list
+*/
+
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+
+
+}
