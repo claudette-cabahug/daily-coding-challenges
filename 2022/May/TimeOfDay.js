2bacb3ad0f307e411ef58e7d72792cd07fe1d866
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-05-30 22:00:32 +1200

    add a function that shows greeting depending on time of day

---
+++ 2022/May/TimeOfDay.js
@@ -0,0 +1,17 @@
+function getGreeting() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
+  console.log(timeOfDay) 
+}
+
+getGreeting()
