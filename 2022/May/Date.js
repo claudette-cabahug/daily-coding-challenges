f1fd3f7091f16d1664131348284c380a946cc148
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-05-31 22:39:06 +1200

    moved files to another folder

--- 2022/May/Date.js
+++
@@ -1,9 +0,0 @@
-
-function getHourToday() {
-  let date = new Date() // Mon May 30 2022 13:33:28 GMT+1200 (New Zealand Standard Time)
-  // let hour = date.getHours() % 12 // for 12-hour time
-  let hour = date.getHours()  // for 24-hour time
-  console.log(hour)
-}
-
-getHourToday()
