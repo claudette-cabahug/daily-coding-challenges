8e379f8f044cf1a8288f0eabf28b317e42f5d980
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-02 10:12:29 +1200

    rename files and add an example of the spread syntax

---
+++ 2022/06 June/SpreadSyntax.js
@@ -0,0 +1,19 @@
+// function sum(x, y, z, n) {
+//   return x + y + z + n
+// }
+
+// console.log(sum(9, 5, 7, 6))  //  27
+// console.log(sum(9, 5, 7, 6, 10))  // 27 - need to add new arg
+
+
+
+let numberStore = [0, 1, 2]
+let newNumber = 12
+numberStore = [...numberStore, newNumber]
+
+console.log(numberStore)  // [0, 1, 2, 12]
+
+
+
+
+
